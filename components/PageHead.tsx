76df62290118ba8c2031f2cf16ea836bd09afc36
joans <155308134+joansnotion@users.noreply.github.com>
2024-04-09 23:35:53 +0800

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -89,7 +89,7 @@ export const PageHead: React.FC<
       <meta name='twitter:title' content={title} />
       <title>{title}</title>
 
-      <script crossOrigin={"anonymous"} async={true} src={"/kodepay/common/kodepay-website.js"}></script>
+      <script defer src={"/kodepay/common/kodepay-website.js"}></script>
     </Head>
   )
 }
